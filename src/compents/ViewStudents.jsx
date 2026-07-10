44017746df6145cbf13548f4f3bbec5fc5195fc3
alantsaji18 <alantsaji10@gmail.com>
2026-07-10 10:18:04 +0530

    JSON updated

--- src/compents/ViewStudents.jsx
+++ src/compents/ViewStudents.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavBar } from './NavBar'
 
 export const ViewStudents = () => {
+
+    const [data,changeData] = useState(
+        [
+            {"name":"Thomas Shelby","avatar":"https://th.bing.com/th/id/OIP.ThkrFok0-n9HkCUe_LGXJAHaNK?w=116&h=187&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3 ","admn":"1105","Class":"MCA"},
+            {"name":"Joker","avatar":" https://th.bing.com/th/id/R.fba6ecd5d2f7c7925177bceb222b09b1?rik=Ghf2DCU4j3SiZQ&riu=http%3a%2f%2fmedia.techeblog.com%2fimages%2fjoaquin-phoenix-joker.jpg&ehk=bl0nW7Dbn5gDa4Dm1mQ1oRYsz38uRyTfPAdVcWu6GvQ%3d&risl=&pid=ImgRaw&r=0","admn":"1305","Class":"MTECH"},
+            {"name":"John Wick","avatar":"https://th.bing.com/th/id/OIP.zMA_TNRNWSWl0ZgnMNUlQwHaDt?w=349&h=175&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3 ","admn":"1102","Class":"MCA"},
+            {"name":"Tony Stark","avatar":" https://th.bing.com/th/id/OIP.uqY8PjmZ9GFiwXqqMc27UAHaHa?w=168&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3","admn":"1205","Class":"MTECH"},
+            {"name":"Steve Rogers","avatar":"https://imageio.forbes.com/specials-images/imageserve/6633ddebcf6f35cb7a555949/Leonardo-DiCaprio-arrives-for-the-92nd-Academy-Awards-/960x0.jpg?format=jpg&width=1440 ","admn":"1805","Class":"MBA"}
+        ]
+
+    )
+
     return (
         <div>
             <NavBar/>
@@ -9,148 +21,33 @@ export const ViewStudents = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                        <div className="row">
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                        <div className="row g-3">
+                            {data.map(
+                                (value,index) => {
+
+                                    return(
+
+                                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
 
                                 <div class="card">
-                                    <img src="https://th.bing.com/th/id/OIP.ThkrFok0-n9HkCUe_LGXJAHaNK?w=116&h=187&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" class="card-img-top" alt="..." height="350" width="50" />
+                                    <img src={value.avatar} class="card-img-top" alt="..." height="350" width="50" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Thomas Shelby</h5>
-                                        <p class="card-text">Admission Number:1105</p>
-                                        <p class="card-text">Class/Grade:MCA</p>
+                                        <h5 class="card-title">{value.name}</h5>
+                                        <p class="card-text">{value.admn}</p>
+                                        <p class="card-text">{value.Class}</p>
                                         <a href="#" class="btn btn-primary">View Details</a>
                                     </div>
                                 </div>
 
 
                             </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                           
 
-                                <div class="card">
-                                    <img src="https://th.bing.com/th/id/R.fba6ecd5d2f7c7925177bceb222b09b1?rik=Ghf2DCU4j3SiZQ&riu=http%3a%2f%2fmedia.techeblog.com%2fimages%2fjoaquin-phoenix-joker.jpg&ehk=bl0nW7Dbn5gDa4Dm1mQ1oRYsz38uRyTfPAdVcWu6GvQ%3d&risl=&pid=ImgRaw&r=0" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Joker</h5>
-                                            <p class="card-text">Admission Number:1305</p>
-                                            <p class="card-text">Class/Grade:MTECH</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
+                                    )
 
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-
-                                <div class="card">
-                                    <img src="https://th.bing.com/th/id/OIP.zMA_TNRNWSWl0ZgnMNUlQwHaDt?w=349&h=175&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">John Wick</h5>
-                                            <p class="card-text">Admission Number:1102</p>
-                                            <p class="card-text">Class/Grade:MCA</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-
-
-                            </div>
-                             <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src=" https://th.bing.com/th/id/OIP.uqY8PjmZ9GFiwXqqMc27UAHaHa?w=168&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Tony Stark</h5>
-                                            <p class="card-text">Admission Number:1205</p>
-                                            <p class="card-text">Class/Grade:MTECH</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://th.bing.com/th/id/OIP.0teCRsAXATeQ3TdC6QGoLgHaK9?w=138&h=204&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Steve Rogers</h5>
-                                            <p class="card-text">Admission Number:1805</p>
-                                            <p class="card-text">Class/Grade:MBA</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-
-
-                            </div>
-                             <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src=" https://imageio.forbes.com/specials-images/imageserve/6633ddebcf6f35cb7a555949/Leonardo-DiCaprio-arrives-for-the-92nd-Academy-Awards-/960x0.jpg?format=jpg&width=1440" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Decaprio</h5>
-                                            <p class="card-text">Admission Number:1905</p>
-                                            <p class="card-text">Class/Grade:MBA</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-
-
-                            </div>
-                             <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://th.bing.com/th/id/OIP.1ZGQSPjCY-hWHbodKj60vQHaLF?w=117&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Mr.Bean</h5>
-                                            <p class="card-text">Admission Number:1005</p>
-                                            <p class="card-text">Class/Grade:MCA</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://tse1.mm.bing.net/th/id/OIP.oxuwMuOuFvcLiIGSk_h8YgHaJS?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Andrew Garified</h5>
-                                            <p class="card-text">Admission Number:1662</p>
-                                            <p class="card-text">Class/Grade:MBA</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-
-
-                            </div>
-                             <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src=" https://tse1.mm.bing.net/th/id/OIP.-l4osUf9ryv_381iuPBt4wAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Stephen</h5>
-                                            <p class="card-text">Admission Number:1725</p>
-                                            <p class="card-text">Class/Grade:MTech</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-
-
-                            </div>
-                             <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src=" https://th.bing.com/th/id/OIP.-_NG4MdRViyY6qtkJUNzDQHaFj?w=244&h=183&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" class="card-img-top" alt="..." height="350" width="50"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Paul Walker</h5>
-                                            <p class="card-text">Admission Number:1825</p>
-                                            <p class="card-text">Class/Grade:MBA</p>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-
-
-                            </div>
+                                }
+                            )}
+                            
                         </div>
 
 
